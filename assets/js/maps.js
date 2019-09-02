94bbeb2d7873b04951ba7c50046bca24e8745857
Ubuntu <ubuntu@ip-172-31-12-55.eu-west-1.compute.internal>
2019-09-02 09:05:43 +0000

    Added checkboxes to the map and adjusted modal box arrangment and color

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -110,7 +110,7 @@
  function search() {
      var search = {
          bounds: map.getBounds(),
-         types: ['lodging restaurant shopping_mall']
+         types: ['lodging restaurant shopping_mall spa art_gallery train_station']
      };
 
      places.nearbySearch(search, function(results, status) {
